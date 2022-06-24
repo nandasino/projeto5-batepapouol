b6decf45f341c1c66a502f5cddb0efcb9b822bab
unknown <fernandamsino@gmail.com>
2022-06-24 17:37:26 -0300

    função de renderizar mensagem testada

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 let mensagens;
 buscarMensagens();
 function buscarMensagens() {
-    console.log("Ordem de execução: 1 - Mensagens");
     const promessa = axios.get(
       "https://mock-api.driven.com.br/api/v6/uol/messages"
     );
-    //Quando essa promessa for resolvida executo o que tá dentro then
     promessa.then(popularMensagens);
   }
   function popularMensagens(resposta) {
-    console.log("Ordem de execução: 2 - popularReceitas()");
     console.log(resposta);
-  
-    //Deu bom na chamada do GET
     if (resposta.status === 200) {
       console.log("Deuuu boooom");
     }
     mensagens= resposta.data;
     console.log(mensagens);
+    renderizarMensagens();
+  }
+  function renderizarMensagens(){
+    const addMsg = document.querySelector(".mensagens");
+    addMsg.innerHTML = "";
+
+    for (let i=0; i< mensagens.length; i++){
+        let tipo = mensagens[i].type;
+        console.log(tipo);
+        if (tipo === "status"){
+            addMsg.innerHTML +=`
+            <h1>Aqui vai um status</h1>
+            `
+        }
+        else if (tipo === "message"){
+            addMsg.innerHTML += `
+            <h1>Aqui vai uma mensagem</h1>            `
+        }
+    }
   }
